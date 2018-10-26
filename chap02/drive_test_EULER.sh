a73ff581ba5a439da964bc6089950c3850deeb60
hexu <hexu_1985@sina.com>
2018-10-26 18:16:26 +0800

    add EULER.hpp and test cpp

---
+++ chap02/drive_test_EULER.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./test_EULER ../chap01_2/graph_1.17.json
---
+++ chap02/test_EULER.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <algorithm>
+#include <iterator>
+#include <cassert>
+
+#ifdef USE_DENSE_GRAPH
+#include "DenseGRAPH.hpp"
+#else
+#include "SparseMultiGRAPH.hpp"
+#endif
+
+#include "IO.hpp"
+#include "JsonFileIO.hpp"
+#include "EULER.hpp"
+#include "get_ord.hpp"
+
+using namespace std;
+using namespace graph_algo;
+
+#ifdef USE_DENSE_GRAPH
+using GRAPH = DenseGRAPH;
+#else
+using GRAPH = SparseMultiGRAPH;
+#endif
+
+int main(int argc, char *argv[])
+{ 
+	if (argc != 2) {
+		cout << "usage: " << argv[0] << " filepath" << endl;
+		exit(1);
+	}
+
+	auto G = JsonFileIO<GRAPH>::loadEZ(argv[1]);
+	cout << "============== graph show ==============\n";
+	IO<GRAPH>::show(*G);
+	cout << endl;
+
+	cout << "total edges: " << G->E() << endl;
+
+	cout << "\nDepth-first search test(EULER)\n";
+    EULER<GRAPH> euler(*G);
+
+    cout << endl;
+    cout << "    |";
+    for (int i = 0; i < G->V(); i++) cout << ' ' << setw(2) << i;
+    cout << '\n';
+
+    cout << "-----";
+    for (int i = 0; i < G->V(); i++) cout << "---";
+    cout << '\n';
+
+    auto ord = get_ord(euler);
+    assert(ord.size() == G->V());
+    cout << "ord |";
+    for (int i = 0; i < G->V(); i++) cout << ' ' << setw(2) << ord[i];
+    cout << '\n';
+
+    cout << endl;
+	return 0;
+}
+
