07287d662f84b8709645406e646ece2a13af51a7
hexu <hexu_1985@sina.com>
2018-11-07 17:44:30 +0800

    add EC_trace.hpp and test cpp

---
+++ chap02/drive_test_EC_trace.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./test_EC_trace ./graph_2.16.json
---
+++ chap02/test_EC_trace.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <algorithm>
+#include <iterator>
+#include <cassert>
+
+#ifdef USE_DENSE_GRAPH
+#include "DenseGRAPH.hpp"
+#else
+#include "SparseMultiGRAPH.hpp"
+#endif
+
+#include "IO.hpp"
+#include "JsonFileIO.hpp"
+#include "EC_trace.hpp"
+#include "get_ord.hpp"
+#include "get_low.hpp"
+
+using namespace std;
+using namespace graph_algo;
+
+#ifdef USE_DENSE_GRAPH
+using GRAPH = DenseGRAPH;
+#else
+using GRAPH = SparseMultiGRAPH;
+#endif
+
+int main(int argc, char *argv[])
+{ 
+	if (argc != 2) {
+		cout << "usage: " << argv[0] << " filepath" << endl;
+		exit(1);
+	}
+
+	auto G = JsonFileIO<GRAPH>::loadEZ(argv[1]);
+	cout << "============== graph show ==============\n";
+	IO<GRAPH>::show(*G);
+	cout << endl;
+
+	cout << G->E() << " edges\n";
+
+	EC<GRAPH> Gec(*G);
+
+	cout << Gec.count() << " edge-connected components" << endl;
+
+	cout << "\n\n";
+    cout << "    |";
+    for (int i = 0; i < G->V(); i++) cout << ' ' << setw(2) << i;
+    cout << '\n';
+
+    cout << "-----";
+    for (int i = 0; i < G->V(); i++) cout << "---";
+    cout << '\n';
+
+    auto ord = get_ord(Gec);
+    assert(ord.size() == G->V());
+    cout << "ord |";
+    for (int i = 0; i < G->V(); i++) cout << ' ' << setw(2) << ord[i];
+    cout << '\n';
+
+    auto low = get_low(Gec);
+    assert(low.size() == G->V());
+    cout << "low |";
+    for (int i = 0; i < G->V(); i++) cout << ' ' << setw(2) << low[i];
+    cout << '\n';
+
+    cout << endl;
+	return 0;
+}
+
