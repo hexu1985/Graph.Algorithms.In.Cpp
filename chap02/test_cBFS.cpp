93f06a5ff7efffe1c02081b7d4f523e33e08833c
hexu <hexu@sensetime.com>
2018-11-14 17:48:22 +0800

    add cBFS.hpp and test cpp

---
+++ chap02/test_cBFS.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <algorithm>
+#include <iterator>
+
+#ifdef USE_DENSE_GRAPH
+#include "DenseGRAPH.hpp"
+#else
+#include "SparseMultiGRAPH.hpp"
+#endif
+
+#include "IO.hpp"
+#include "JsonFileIO.hpp"
+#include "cBFS.hpp"
+#include "get_ord.hpp"
+#include "inverseMap.hpp"
+
+using namespace std;
+using namespace graph_algo;
+
+#ifdef USE_DENSE_GRAPH
+using GRAPH = DenseGRAPH;
+#else
+using GRAPH = SparseMultiGRAPH;
+#endif
+
+int main(int argc, char *argv[])
+{ 
+	if (argc != 2) {
+		cout << "usage: " << argv[0] << " filepath" << endl;
+		exit(1);
+	}
+
+	auto G = JsonFileIO<GRAPH>::loadEZ(argv[1]);
+	cout << "============== graph show ==============\n";
+	IO<GRAPH>::show(*G);
+	cout << endl;
+
+	cout << "total edges: " << G->E() << endl;
+
+	cout << "Depth-first search test, cin vertex search begin with, like: 0 (entry negative to exit)\n";
+	while (true) {
+		cout << "$ ";
+		int v = 0;
+//		cin >> v;
+		if (v < 0 || !cin)
+			break;
+		if (v >= G->V()) {
+			cout << "invalid vertex\n";
+			continue;
+		}
+
+        cBFS<GRAPH> bfs(*G, v);
+		cout << "\nvisit sequence of bfs begin with vertex " << v << ": " << endl;
+        auto vec = inverseMap(get_ord(bfs));
+        copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, ", "));
+        cout << "\n";
+		break;
+	}
+
+    cout << endl;
+
+	return 0;
+}
+
---
+++ include/cBFS.hpp
@@ -0,0 +1,44 @@
+#ifndef GRAPH_ALGO_CBFS_HPP
+#define GRAPH_ALGO_CBFS_HPP
+
+#include <vector>
+#include "QUEUE.hpp"
+
+namespace graph_algo {
+
+template <class Graph> 
+class cBFS { 
+private:
+    const Graph &G;
+    int cnt = 0;
+    std::vector<int> ord; 
+
+    void searchC(int v)
+    { 
+        QUEUE<int> Q;  
+        Q.put(v);
+		while (!Q.empty()) 
+			if (ord[v= Q.get()] == -1)
+			{
+				ord[v] = cnt++;
+				typename Graph::adjIterator A(G, v);
+				for (int t = A.beg(); !A.end(); t = A.nxt())
+					if (ord[t] == -1) Q.put(t);
+			}
+    }
+
+public:
+    cBFS(const Graph &G, int v = 0) : 
+        G(G), cnt(0), ord(G.V(), -1) 
+    { searchC(v); }
+
+    int count() const { return cnt; }
+    int operator [](int v) const { return ord[v]; }
+
+    template <class T>
+    friend const std::vector<int> &get_ord(const T &t);
+};
+
+}	// namespace
+
+#endif
