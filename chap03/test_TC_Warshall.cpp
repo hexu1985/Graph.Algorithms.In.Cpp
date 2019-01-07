74bfeca498be2fd4af45a80079b3df4b932e9d68
hexu <hexu_1985@sina.com>
2019-01-07 18:26:31 +0800

    add TC Warshall algorithm and demo

---
+++ chap03/test_TC_Warshall.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include "GRAPH.hpp"
+#include "IO.hpp"
+#include "JsonFileIO.hpp"
+#include "TC_Warshall.hpp"
+
+using namespace std;
+using namespace graph_algo;
+
+int main(int argc, char *argv[])
+{ 
+	if (argc != 2) {
+		cout << "usage: " << argv[0] << " filepath" << endl;
+		exit(1);
+	}
+
+	auto G = JsonFileIO<GRAPH>::loadEZ(argv[1]);
+	cout << "============== graph show ==============\n";
+    IO<GRAPH>::showAdjMatrix(*G);
+	cout << endl;
+
+	cout << "total edges: " << G->E() << endl;
+
+	cout << "\nTransitive Closure with Warshall's algorithm test\n";
+    TC<GRAPH, GRAPH> tc(*G);
+
+	cout << "============== TC graph show ==============\n";
+    IO<GRAPH>::showAdjMatrix(get_Graph_TC(tc));
+	cout << endl;
+
+	cout << "total edges: " << get_Graph_TC(tc).E() << endl;
+
+	return 0;
+}
+
---
+++ include/TC_Warshall.hpp
@@ -0,0 +1,41 @@
+#ifndef GRAPH_ALGO_TC_WARSHALL_HPP
+#define GRAPH_ALGO_TC_WARSHALL_HPP
+
+#include "Edge.hpp"
+
+namespace graph_algo {
+
+template <class tcGraph, class Graph> 
+class TC { 
+    tcGraph T;
+
+public:
+    TC(const Graph &G) : T(G) 
+    { 
+        for (int s = 0; s < T.V(); s++) 
+            T.insert(Edge(s, s));
+        for (int i = 0; i < T.V(); i++)
+            for (int s = 0; s < T.V(); s++)
+                if (T.edge(s, i))
+                    for (int t = 0; t < T.V(); t++)
+                        if (T.edge(i, t))
+                            T.insert(Edge(s, t));
+    }
+
+    bool reachable(int s, int t) const
+    { return T.edge(s, t); }
+
+    template <class TcG, class G>
+    friend const TcG &get_Graph_TC(const TC<TcG, G> &tc);
+};
+
+template <class TcG, class G>
+const TcG &get_Graph_TC(const TC<TcG, G> &tc)
+{
+    return tc.T;
+}
+
+}	// namespace
+
+#endif
+
